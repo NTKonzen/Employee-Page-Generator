30ec421ccfd2fa6d707081bb42bcec4c6f92894b
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-09-04 18:47:56 -0700

    Created getID and getEmail methods under Employee class and associated tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,7 +3,17 @@ const Employee = require('../lib/Employee.js')
 describe('Employee Class', () => {
     describe('getName', () => {
         it('returns the employee\'s name', () => {
-            expect(new Employee('jim', 1, 'email').getName()).toBe
+            expect(new Employee('jim', 1, 'email').getName()).toBe('jim')
+        })
+    })
+    describe('getID', () => {
+        it('returns the employee\'s id', () => {
+            expect(new Employee('jim', 1, 'email').getID()).toBe(1)
+        })
+    })
+    describe('getEmail', () => {
+        it('returns the employee\'s email', () => {
+            expect(new Employee('jim', 1, 'email').getEmail()).toBe('email')
         })
     })
 })
