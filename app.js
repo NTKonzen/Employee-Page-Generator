b7674dc681e2436705c8429bc564c9cedd4422b2
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-09-08 18:14:52 -0700

    Got output working properly

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ const Templates = require('./templates/templates')
 
 const employeeArray = []
 
-const ryan = new Manager('Ryan', 1, 2, 'email@email.com')
-const matthew = new Manager('Matthew', 12, 444, 'email@email.com')
+const ryan = new Manager('Ryan', 1, 'email@email.com', 2)
+const matthew = new Manager('Matthew', 12, 'email@email.com', 444)
 employeeArray.push(matthew)
 employeeArray.push(ryan)
-employeeArray.push(new Engineer('Jessica', 1828685, 'JSOolong', 'email@email.com'))
-employeeArray.push(new Intern('Rebecca', 9999, 'Stanford Univeristy', 'email@email.com'))
+employeeArray.push(new Engineer('Jessica', 1828685, 'email@email.com', 'JSOolong'))
+employeeArray.push(new Intern('Rebecca', 9999, 'email@email.com', 'Stanford University'))
 
 console.log(employeeArray)
 
-employeeArray.forEach(employeeObj => {
+employeeArray.forEach((employeeObj, index) => {
+    console.log(index)
     if (employeeObj.getRole() === 'Manager') {
+        employeeArray[index] = Templates.getManager(employeeObj.name, employeeObj.id, employeeObj.officeNumber, employeeObj.email)
         console.log(employeeObj.getRole())
     } else if (employeeObj.getRole() === 'Engineer') {
+        employeeArray[index] = Templates.getEngineer(employeeObj.name, employeeObj.id, employeeObj.github, employeeObj.email)
         console.log(employeeObj.getRole())
     } else if (employeeObj.getRole() === 'Intern') {
+        employeeArray[index] = Templates.getIntern(employeeObj.name, employeeObj.id, employeeObj.school, employeeObj.email)
         console.log(employeeObj.getRole())
     }
 })
+
+console.log(Templates.generatePage(employeeArray))
