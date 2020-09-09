2d2bf363e7ff786de77a84bcaa392295375ee4c6
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-09-08 18:36:16 -0700

    Created CLI prompt for adding new managers

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
@@ -5,14 +8,38 @@ const Templates = require('./templates/templates')
 
 const employeeArray = []
 
-const ryan = new Manager('Ryan', 1, 'email@email.com', 2)
-const matthew = new Manager('Matthew', 12, 'email@email.com', 444)
-employeeArray.push(matthew)
-employeeArray.push(ryan)
-employeeArray.push(new Engineer('Jessica', 1828685, 'email@email.com', 'JSOolong'))
-employeeArray.push(new Intern('Rebecca', 9999, 'email@email.com', 'Stanford University'))
+async function getManagers() {
+    do {
+        let newMan = await inquirer.prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: 'Enter the name of this manager: \n'
+            }, {
+                name: 'id',
+                type: 'input',
+                message: 'Enter this manager\'s id: \n'
+            }, {
+                name: 'email',
+                type: 'input',
+                message: 'Enter this manager\'s office number: \n'
+            }, {
+                name: 'officeNumber',
+                type: 'input',
+                message: 'Enter this manager\s office number'
+            }
+        ])
+        var more = await inquirer.prompt({
+            name: 'more',
+            type: 'confirm',
+            message: 'Do you want to add another manager?\n'
+        })
+        console.log(newMan)
+        console.log(more.more)
+    } while (more.more === true)
+}
 
-console.log(employeeArray)
+getManagers()
 
 employeeArray.forEach((employeeObj, index) => {
     console.log(index)
@@ -27,5 +54,3 @@ employeeArray.forEach((employeeObj, index) => {
         console.log(employeeObj.getRole())
     }
 })
-
-console.log(Templates.generatePage(employeeArray))
