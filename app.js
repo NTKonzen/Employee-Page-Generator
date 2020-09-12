f09af7bc6083e1466d5e334acfdb6225619d4fb1
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-09-12 14:22:55 -0700

    generated Manager html is pushed to the employeeArray

--- app.js
+++ app.js
@@ -34,23 +34,30 @@ async function getManagers() {
             type: 'confirm',
             message: 'Do you want to add another manager?\n'
         })
-        console.log(newMan)
-        console.log(more.more)
+        employeeArray.push(new Manager(newMan.name, newMan.id, newMan.email, newMan.officeNumber))
     } while (more.more === true)
 }
 
-getManagers()
+async function main() {
 
-employeeArray.forEach((employeeObj, index) => {
-    console.log(index)
-    if (employeeObj.getRole() === 'Manager') {
-        employeeArray[index] = Templates.getManager(employeeObj.name, employeeObj.id, employeeObj.officeNumber, employeeObj.email)
-        console.log(employeeObj.getRole())
-    } else if (employeeObj.getRole() === 'Engineer') {
-        employeeArray[index] = Templates.getEngineer(employeeObj.name, employeeObj.id, employeeObj.github, employeeObj.email)
-        console.log(employeeObj.getRole())
-    } else if (employeeObj.getRole() === 'Intern') {
-        employeeArray[index] = Templates.getIntern(employeeObj.name, employeeObj.id, employeeObj.school, employeeObj.email)
-        console.log(employeeObj.getRole())
-    }
-})
+    await getManagers()
+
+    employeeArray.forEach((employeeObj, index) => {
+        console.log(index)
+        if (employeeObj.getRole() === 'Manager') {
+            employeeArray[index] = Templates.getManager(employeeObj.name, employeeObj.id, employeeObj.officeNumber, employeeObj.email)
+        } else if (employeeObj.getRole() === 'Engineer') {
+            employeeArray[index] = Templates.getEngineer(employeeObj.name, employeeObj.id, employeeObj.github, employeeObj.email)
+            console.log(employeeObj.getRole())
+        } else if (employeeObj.getRole() === 'Intern') {
+            employeeArray[index] = Templates.getIntern(employeeObj.name, employeeObj.id, employeeObj.school, employeeObj.email)
+            console.log(employeeObj.getRole())
+        }
+    })
+
+
+}
+
+main()
+
+
